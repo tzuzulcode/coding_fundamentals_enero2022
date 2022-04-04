0164faf50e975282ed4a7c59f73126acea1b9220
Tzuzul Code <mail@tzuzulcode.com>
2022-04-04 07:08:44 -0500

    Promises

---
+++ Async/promises.js
@@ -0,0 +1,47 @@
+const app = document.getElementById("app")
+
+// URL: https://rickandmortyapi.com/api
+
+// const resultado = fetch("https://rickandmortyapi.com/api") // fetch significa dar la orden de traernos algo
+
+// console.log(resultado)
+
+// fetch("https://rickandmortyapi.com/api")
+// .then((value)=>{
+//     value.json()
+//     .then((datos)=>{
+//         console.log(da)
+//     })
+
+// }) // Recibe un callback. Nos indica que la promesa se cumplio
+// .catch(function (error){
+//     console.log(error)
+//     const mensaje = document.createElement("p")
+//     mensaje.innerText = "Ocurrio un error al pedir la información"
+//     app.appendChild(mensaje)
+// })
+// fetch("https://rickandmortyapi.com/api")
+// .then((value)=>{
+//     return value.json()
+// }).then((datos)=>{
+//     console.log(datos)
+// })
+// .catch(function (error){
+//     console.log(error)
+//     const mensaje = document.createElement("p")
+//     mensaje.innerText = "Ocurrio un error al pedir la información"
+//     app.appendChild(mensaje)
+// })
+
+
+fetch("https://rickandmortyapi.com/api")
+.then(value=>value.json()) // Conversion de stream a JSON. Devuelve otra promesa
+.then(datos=>{
+    console.log(datos) // Imprimimos resultados
+})
+.catch(error=>{
+    console.log(error)
+    const mensaje = document.createElement("p")
+    mensaje.innerText = "Ocurrio un error al pedir la información"
+    app.appendChild(mensaje)
+}) // Gestionamos el error
